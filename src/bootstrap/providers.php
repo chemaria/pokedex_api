537efd977e2b1b6226ec6f353c7a222a7e950115
Chema <josecruzigle@gmail.com>
2025-09-14 09:43:36 +0200

    feat(laravel/bootstrap): Add SharedServiceProvider to providers list

--- src/bootstrap/providers.php
+++ src/bootstrap/providers.php
@@ -2,5 +2,6 @@
 
 return [
     App\Providers\AppServiceProvider::class,
+    App\Shared\Infrastructure\Providers\SharedServiceProvider::class,
     App\Pokemon\Infrastructure\Providers\PokemonServiceProvider::class,
 ];
